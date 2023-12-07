27649765a52a2b78e3517ededc1b4e97369954e0
Roger Mc Murtrie <rogermc@grapevine.com.au>
2023-12-08 07:32:48 +1100

    Update

--- src/commands_and_tokens_tables.ads
+++ src/commands_and_tokens_tables.ads
@@ -5,7 +5,17 @@ with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 
 package Commands_And_Tokens_Tables is
 
-   type Fun_Type is (T_FNA, T_FUN, T_INT, T_NBR, T_STR);
+   type Fun_Type is (T_NONE, T_FNA, T_FUN, T_INT, T_NBR, T_STR);
+
+   type Command_Table_Item is record
+      Name          : Unbounded_String;
+      Command_Type1 : Fun_Type := T_NONE;
+      Command_Type2 : Fun_Type := T_NONE;
+      Command_Type3 : Fun_Type := T_NONE;
+      Command_Type4 : Fun_Type := T_NONE;
+      Precedence    : Natural := 0;
+      Function_Ptr  : Address := Null_Address;
+   end record;
 
    type Token_Table_Item is record
       Name         : Unbounded_String;
@@ -13,8 +23,7 @@ package Commands_And_Tokens_Tables is
       Token_Type   : Integer := 0;
       Function_Ptr : Address := Null_Address;
    end record;
-
-   Command_Table_Size : constant Positive := 44;
+   Command_Table_Size : constant Positive := 45;
    Token_Table_Size   : constant Positive := 45;
 
    fun_acos              : constant Address := Null_Address;
@@ -36,6 +45,7 @@ package Commands_And_Tokens_Tables is
    fun_instr             : constant Address := Null_Address;
    fun_int               : constant Address := Null_Address;
    fun_lcase             : constant Address := Null_Address;
+   fun_ucase             : constant Address := Null_Address;
    fun_left              : constant Address := Null_Address;
    fun_len               : constant Address := Null_Address;
    fun_log               : constant Address := Null_Address;
@@ -49,6 +59,7 @@ package Commands_And_Tokens_Tables is
    fun_rnd               : constant Address := Null_Address;
    fun_sgn               : constant Address := Null_Address;
    fun_sin               : constant Address := Null_Address;
+   fun_tan               : constant Address := Null_Address;
    fun_space             : constant Address := Null_Address;
    fun_sqr               : constant Address := Null_Address;
    fun_str               : constant Address := Null_Address;
@@ -61,96 +72,96 @@ package Commands_And_Tokens_Tables is
    fun_max               : constant Address := Null_Address;
    fun_min               : constant Address := Null_Address;
 
-   Command_Table : array (1 .. Command_Table_Size) of Token_Table_Item :=
-                     ((To_Unbounded_String ("ACos(To_Unbounded_String ("),  T_FUN or T_NBR, 0, fun_acos),
-                      (To_Unbounded_String ("Abs(To_Unbounded_String ("),   T_FUN or T_NBR or T_INT, 0, fun_abs),
-                      (To_Unbounded_String ("Asc(To_Unbounded_String ("),   T_FUN or T_INT, 0, fun_asc),
-                      (To_Unbounded_String ("ASin(To_Unbounded_String ("),  T_FUN or T_NBR, 0, fun_asin),
-                      (To_Unbounded_String ("Atn(To_Unbounded_String ("),   T_FUN or T_NBR, 0, fun_atn),
-                      (To_Unbounded_String ("Bin$(To_Unbounded_String ("),  T_FUN or T_STR, 0, fun_bin),
-                      (To_Unbounded_String ("Chr$(To_Unbounded_String ("),  T_FUN or T_STR, 0, fun_chr),
-                      (To_Unbounded_String ("Cint(To_Unbounded_String ("),  T_FUN or T_INT, 0, fun_cint),
-                      (To_Unbounded_String ("Cos(To_Unbounded_String ("),   T_FUN or T_NBR, 0, fun_cos),
-                      (To_Unbounded_String ("Deg(To_Unbounded_String ("),   T_FUN or T_NBR, 0, fun_deg),
-                      (To_Unbounded_String ("MM.Errno"),   T_FNA or T_INT, 0, fun_errno),
-                      (To_Unbounded_String ("MM.ErrMsg$"), T_FNA or T_STR, 0, fun_errmsg),
-                      (To_Unbounded_String ("Exp(To_Unbounded_String ("),   T_FUN or T_NBR, 0, fun_exp),
-                      (To_Unbounded_String ("Fix(To_Unbounded_String ("),   T_FUN or T_INT, 0, fun_fix),
-                      (To_Unbounded_String ("Hex$(To_Unbounded_String ("),  T_FUN or T_STR, 0, fun_hex),
-                      (To_Unbounded_String ("Inkey$"), T_FNA or T_STR, 0, fun_inkey),
-                      (To_Unbounded_String ("Instr(To_Unbounded_String ("), T_FUN or T_INT, 0, fun_instr),
-                      (To_Unbounded_String ("Int(To_Unbounded_String ("),   T_FUN or T_INT, 0, fun_int),
-                      (To_Unbounded_String ("LCase$(To_Unbounded_String ("),    T_FUN or T_STR, 0, fun_lcase),
-                      (To_Unbounded_String ("Left$(To_Unbounded_String ("), T_FUN or T_STR, 0, fun_left),
-                      (To_Unbounded_String ("Len(To_Unbounded_String ("),   T_FUN or T_INT, 0, fun_len),
-                      (To_Unbounded_String ("Log(To_Unbounded_String ("),   T_FUN or T_NBR, 0, fun_log),
-                      (To_Unbounded_String ("Mid$(To_Unbounded_String ("),  T_FUN or T_STR, 0, fun_mid),
-                      (To_Unbounded_String ("MM.Ver"), T_FNA or T_NBR, 0, fun_version),
-                      (To_Unbounded_String ("Oct$(To_Unbounded_String ("),  T_FUN or T_STR, 0, fun_oct),
-                      (To_Unbounded_String ("Pi"),     T_FNA or T_NBR, 0, fun_pi ),
-                      (To_Unbounded_String ("Pos"),    T_FNA or T_INT, 0, fun_pos),
-                      (To_Unbounded_String ("Rad(To_Unbounded_String ("),   T_FUN or T_NBR, 0, fun_rad),
-                      (To_Unbounded_String ("Right$(To_Unbounded_String ("), T_FUN or T_STR, 0, fun_right),
-                      (To_Unbounded_String ("Rnd(To_Unbounded_String ("),   T_FUN or T_NBR, 0, fun_rnd),  -- this must come before Rnd - without bracket
-                      (To_Unbounded_String ("Rnd"),    T_FNA or T_NBR, 0, fun_rnd),   -- this must come after Rnd(
-                      (To_Unbounded_String ("Sgn(To_Unbounded_String ("),   T_FUN or T_INT, 0, fun_sgn),
-                      (To_Unbounded_String ("Sin(To_Unbounded_String ("),   T_FUN or T_NBR, 0, fun_sin),
-                      (To_Unbounded_String ("Space$(To_Unbounded_String ("), T_FUN or T_STR, 0, fun_space),
-                      (To_Unbounded_String ("Spc(To_Unbounded_String ("),   T_FUN or T_STR, 0, fun_space),
-                      (To_Unbounded_String ("Sqr(To_Unbounded_String ("),   T_FUN or T_NBR, 0, fun_sqr),
-                      (To_Unbounded_String ("Str$(To_Unbounded_String ("),  T_FUN or T_STR, 0, fun_str),
-                      (To_Unbounded_String ("String$(To_Unbounded_String ("), T_FUN or T_STR, 0, fun_string),
-                      (To_Unbounded_String ("Tab(To_Unbounded_String ("),   T_FUN or T_STR, 0, fun_tab),
-                      (To_Unbounded_String ("Tan(To_Unbounded_String ("),   T_FUN or T_NBR, 0, fun_tan),
-                      (To_Unbounded_String ("UCase$(To_Unbounded_String ("), T_FUN or T_STR, 0, fun_ucase),
-                      (To_Unbounded_String ("Val(To_Unbounded_String ("),   T_FUN or T_NBR or T_INT, 0, fun_val),
-                      (To_Unbounded_String ("Eval(To_Unbounded_String ("),  T_FUN or T_NBR or T_INT or T_STR, 0, fun_eval),
-                      (To_Unbounded_String ("Max(To_Unbounded_String ("),   T_FUN or T_NBR, 0, fun_max),
-                      (To_Unbounded_String ("Min(To_Unbounded_String ("),   T_FUN or T_NBR, 0, fun_min));
+   Command_Table : array (1 .. Command_Table_Size) of Command_Table_Item :=
+                     ((To_Unbounded_String ("ACos("),  T_FUN, T_NBR, T_NONE, T_NONE, 0, fun_acos),
+                      (To_Unbounded_String ("Abs("),   T_FUN, T_NBR, T_INT, T_NONE, 0, fun_abs),
+                      (To_Unbounded_String ("Asc("),   T_FUN, T_INT, T_NONE, T_NONE, 0, fun_asc),
+                      (To_Unbounded_String ("ASin("),  T_FUN, T_NBR, T_NONE, T_NONE, 0, fun_asin),
+                      (To_Unbounded_String ("Atn("),   T_FUN, T_NBR, T_NONE, T_NONE, 0, fun_atn),
+                      (To_Unbounded_String ("Bin$("),  T_FUN, T_STR, T_NONE, T_NONE, 0, fun_bin),
+                      (To_Unbounded_String ("Chr$("),  T_FUN, T_STR, T_NONE, T_NONE, 0, fun_chr),
+                      (To_Unbounded_String ("Cint("),  T_FUN, T_INT, T_NONE, T_NONE, 0, fun_cint),
+                      (To_Unbounded_String ("Cos("),   T_FUN, T_NBR, T_NONE, T_NONE, 0, fun_cos),
+                      (To_Unbounded_String ("Deg("),   T_FUN, T_NBR, T_NONE, T_NONE, 0, fun_deg),
+                      (To_Unbounded_String ("MM.Errno"),   T_FNA, T_INT, T_NONE, T_NONE, 0, fun_errno),
+                      (To_Unbounded_String ("MM.ErrMsg$"), T_FNA, T_STR, T_NONE, T_NONE, 0, fun_errmsg),
+                      (To_Unbounded_String ("Exp("),   T_FUN, T_NBR, T_NONE, T_NONE, 0, fun_exp),
+                      (To_Unbounded_String ("Fix("),   T_FUN, T_INT, T_NONE, T_NONE, 0, fun_fix),
+                      (To_Unbounded_String ("Hex$("),  T_FUN, T_STR, T_NONE, T_NONE, 0, fun_hex),
+                      (To_Unbounded_String ("Inkey$"), T_FNA, T_STR, T_NONE, T_NONE, 0, fun_inkey),
+                      (To_Unbounded_String ("Instr("), T_FUN, T_INT, T_NONE, T_NONE, 0, fun_instr),
+                      (To_Unbounded_String ("Int("),   T_FUN, T_INT, T_NONE, T_NONE, 0, fun_int),
+                      (To_Unbounded_String ("LCase$("), T_FUN, T_STR, T_NONE, T_NONE, 0, fun_lcase),
+                      (To_Unbounded_String ("Left$("), T_FUN, T_STR, T_NONE, T_NONE, 0, fun_left),
+                      (To_Unbounded_String ("Len("),   T_FUN, T_INT, T_NONE, T_NONE, 0, fun_len),
+                      (To_Unbounded_String ("Log("),   T_FUN, T_NBR, T_NONE, T_NONE, 0, fun_log),
+                      (To_Unbounded_String ("Mid$("),  T_FUN, T_STR, T_NONE, T_NONE, 0, fun_mid),
+                      (To_Unbounded_String ("MM.Ver"), T_FNA, T_NBR, T_NONE, T_NONE, 0, fun_version),
+                      (To_Unbounded_String ("Oct$("),  T_FUN, T_STR, T_NONE, T_NONE, 0, fun_oct),
+                      (To_Unbounded_String ("Pi"),     T_FNA, T_NBR, T_NONE, T_NONE, 0, fun_pi ),
+                      (To_Unbounded_String ("Pos"),    T_FNA, T_INT, T_NONE, T_NONE, 0, fun_pos),
+                      (To_Unbounded_String ("Rad("),   T_FUN, T_NBR, T_NONE, T_NONE, 0, fun_rad),
+                      (To_Unbounded_String ("Right$("), T_FUN, T_STR, T_NONE, T_NONE, 0, fun_right),
+                      (To_Unbounded_String ("Rnd("),   T_FUN, T_NBR, T_NONE, T_NONE, 0, fun_rnd),  -- this must come before Rnd - without bracket
+                      (To_Unbounded_String ("Rnd"),    T_FNA, T_NBR, T_NONE, T_NONE, 0, fun_rnd),   -- this must come after Rnd(
+                      (To_Unbounded_String ("Sgn("),   T_FUN, T_INT, T_NONE, T_NONE, 0, fun_sgn),
+                      (To_Unbounded_String ("Sin("),   T_FUN, T_NBR, T_NONE, T_NONE, 0, fun_sin),
+                      (To_Unbounded_String ("Space$("), T_FUN, T_STR, T_NONE, T_NONE, 0, fun_space),
+                      (To_Unbounded_String ("Spc("),   T_FUN, T_STR, T_NONE, T_NONE, 0, fun_space),
+                      (To_Unbounded_String ("Sqr("),   T_FUN, T_NBR, T_NONE, T_NONE, 0, fun_sqr),
+                      (To_Unbounded_String ("Str$("),  T_FUN, T_STR, T_NONE, T_NONE, 0, fun_str),
+                      (To_Unbounded_String ("String$(("), T_FUN, T_STR, T_NONE, T_NONE, 0, fun_string),
+                      (To_Unbounded_String ("Tab("),   T_FUN, T_STR, T_NONE, T_NONE, 0, fun_tab),
+                      (To_Unbounded_String ("Tan("),   T_FUN, T_NBR, T_NONE, T_NONE, 0, fun_tan),
+                      (To_Unbounded_String ("UCase$("), T_FUN, T_STR, T_NONE, T_NONE, 0, fun_ucase),
+                      (To_Unbounded_String ("Val("),   T_FUN, T_NBR, T_INT, T_NONE, 0, fun_val),
+                      (To_Unbounded_String ("Eval("),  T_FUN, T_NBR, T_INT, T_STR, 0, fun_eval),
+                      (To_Unbounded_String ("Max("),   T_FUN, T_NBR, T_NONE, T_NONE, 0, fun_max),
+                      (To_Unbounded_String ("Min("),   T_FUN, T_NBR, T_NONE, T_NONE, 0, fun_min));
 
-                      Token_Table           : array (1 .. Token_Table_Size) of Token_Table_Item :=
-                      ((To_Unbounded_String ("fun_abs"), 0, 0, Null_Address),
-                         (To_Unbounded_String ("fun_asc"), 0, 0, Null_Address),
-                         (To_Unbounded_String ("fun_atn"), 0, 0, Null_Address),
-                         (To_Unbounded_String ("fun_bin"), 0, 0, Null_Address),
-                         (To_Unbounded_String ("fun_chr"), 0, 0, Null_Address),
-                         (To_Unbounded_String ("fun_cint"), 0, 0, Null_Address),
-                         (To_Unbounded_String ("fun_cos"), 0, 0, Null_Address),
-                         (To_Unbounded_String ("fun_deg"), 0, 0, Null_Address),
-                         (To_Unbounded_String ("fun_errno"), 0, 0, Null_Address),
-                         (To_Unbounded_String ("fun_errmsg"), 0, 0, Null_Address),
-                         (To_Unbounded_String ("fun_exp"), 0, 0, Null_Address),
-                         (To_Unbounded_String ("fun_fix"), 0, 0, Null_Address),
-                         (To_Unbounded_String ("fun_hex"), 0, 0, Null_Address),
-                         (To_Unbounded_String ("fun_inkey"), 0, 0, Null_Address),
-                         (To_Unbounded_String ("fun_instr"), 0, 0, Null_Address),
-                         (To_Unbounded_String ("fun_int"), 0, 0, Null_Address),
-                         (To_Unbounded_String ("fun_lcase"), 0, 0, Null_Address),
-                         (To_Unbounded_String ("fun_left"), 0, 0, Null_Address),
-                         (To_Unbounded_String ("fun_len"), 0, 0, Null_Address),
-                         (To_Unbounded_String ("fun_log"), 0, 0, Null_Address),
-                         (To_Unbounded_String ("fun_mid"), 0, 0, Null_Address),
-                         (To_Unbounded_String ("fun_oct"), 0, 0, Null_Address),
-                         (To_Unbounded_String ("fun_pi"), 0, 0, Null_Address),
-                         (To_Unbounded_String ("fun_pos"), 0, 0, Null_Address),
-                         (To_Unbounded_String ("fun_rad"), 0, 0, Null_Address),
-                         (To_Unbounded_String ("fun_right"), 0, 0, Null_Address),
-                         (To_Unbounded_String ("fun_rnd"), 0, 0, Null_Address),
-                         (To_Unbounded_String ("fun_sgn"), 0, 0, Null_Address),
-                         (To_Unbounded_String ("fun_sin"), 0, 0, Null_Address),
-                         (To_Unbounded_String ("fun_space"), 0, 0, Null_Address),
-                         (To_Unbounded_String ("fun_sqr"), 0, 0, Null_Address),
-                         (To_Unbounded_String ("fun_str"), 0, 0, Null_Address),
-                         (To_Unbounded_String ("fun_string"), 0, 0, Null_Address),
-                         (To_Unbounded_String ("fun_tab"), 0, 0, Null_Address),
-                         (To_Unbounded_String ("fun_tan"), 0, 0, Null_Address),
-                         (To_Unbounded_String ("fun_ucase"), 0, 0, Null_Address),
-                         (To_Unbounded_String ("fun_val"), 0, 0, Null_Address),
-                         (To_Unbounded_String ("fun_eval"), 0, 0, Null_Address),
-                         (To_Unbounded_String ("fun_asin"), 0, 0, Null_Address),
-                         (To_Unbounded_String ("fun_acos"), 0, 0, Null_Address),
-                         (To_Unbounded_String ("fun_max"), 0, 0, Null_Address),
-                         (To_Unbounded_String ("fun_min"), 0, 0, Null_Address),
-                         (To_Unbounded_String (""), 0, 0, Null_Address));
+   Token_Table           : array (1 .. Token_Table_Size) of Token_Table_Item :=
+                             ((To_Unbounded_String ("fun_abs"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("fun_asc"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("fun_atn"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("fun_bin"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("fun_chr"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("fun_cint"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("fun_cos"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("fun_deg"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("fun_errno"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("fun_errmsg"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("fun_exp"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("fun_fix"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("fun_hex"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("fun_inkey"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("fun_instr"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("fun_int"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("fun_lcase"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("fun_left"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("fun_len"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("fun_log"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("fun_mid"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("fun_oct"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("fun_pi"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("fun_pos"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("fun_rad"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("fun_right"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("fun_rnd"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("fun_sgn"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("fun_sin"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("fun_space"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("fun_sqr"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("fun_str"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("fun_string"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("fun_tab"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("fun_tan"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("fun_ucase"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("fun_val"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("fun_eval"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("fun_asin"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("fun_acos"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("fun_max"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("fun_min"), 0, 0, Null_Address),
+                              (To_Unbounded_String (""), 0, 0, Null_Address));
 
-                      end Commands_And_Tokens_Tables;
+end Commands_And_Tokens_Tables;
