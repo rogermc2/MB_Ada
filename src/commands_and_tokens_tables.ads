849eb76d5e3c94296d2d4d01602fcfdf1e27d52a
Roger Mc Murtrie <rogermc@grapevine.com.au>
2023-12-08 11:55:32 +1100

    Update

--- src/commands_and_tokens_tables.ads
+++ src/commands_and_tokens_tables.ads
@@ -23,8 +23,8 @@ package Commands_And_Tokens_Tables is
       Token_Type   : Integer := 0;
       Function_Ptr : Address := Null_Address;
    end record;
-   Command_Table_Size : constant Positive := 45;
-   Token_Table_Size   : constant Positive := 45;
+   Command_Table_Size : constant Positive := 46;
+   Token_Table_Size   : constant Positive := 44 + 37 + 20 + 1;
 
    fun_acos              : constant Address := Null_Address;
    fun_abs               : constant Address := Null_Address;
@@ -72,6 +72,8 @@ package Commands_And_Tokens_Tables is
    fun_max               : constant Address := Null_Address;
    fun_min               : constant Address := Null_Address;
 
+   cmd_nul               : constant Address := Null_Address;
+
    Command_Table : array (1 .. Command_Table_Size) of Command_Table_Item :=
                      ((To_Unbounded_String ("ACos("),  T_FUN, T_NBR, T_NONE, T_NONE, 0, fun_acos),
                       (To_Unbounded_String ("Abs("),   T_FUN, T_NBR, T_INT, T_NONE, 0, fun_abs),
@@ -117,7 +119,8 @@ package Commands_And_Tokens_Tables is
                       (To_Unbounded_String ("Val("),   T_FUN, T_NBR, T_INT, T_NONE, 0, fun_val),
                       (To_Unbounded_String ("Eval("),  T_FUN, T_NBR, T_INT, T_STR, 0, fun_eval),
                       (To_Unbounded_String ("Max("),   T_FUN, T_NBR, T_NONE, T_NONE, 0, fun_max),
-                      (To_Unbounded_String ("Min("),   T_FUN, T_NBR, T_NONE, T_NONE, 0, fun_min));
+                      (To_Unbounded_String ("Min("),   T_FUN, T_NBR, T_NONE, T_NONE, 0, fun_min),
+                      (To_Unbounded_String (""),       T_NONE, T_NONE, T_NONE, T_NONE, 0, cmd_nul));
 
    Token_Table           : array (1 .. Token_Table_Size) of Token_Table_Item :=
                              ((To_Unbounded_String ("fun_abs"), 0, 0, Null_Address),
@@ -162,6 +165,64 @@ package Commands_And_Tokens_Tables is
                               (To_Unbounded_String ("fun_acos"), 0, 0, Null_Address),
                               (To_Unbounded_String ("fun_max"), 0, 0, Null_Address),
                               (To_Unbounded_String ("fun_min"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("cmd_clear"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("cmd_continue"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("cmd_dim"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("cmd_do"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("cmd_else"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("cmd_end"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("cmd_endfun"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("cmd_endsub"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("cmd_erase"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("cmd_error"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("cmd_exit"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("cmd_exitfor"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("cmd_for"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("cmd_subfun"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("cmd_gosub"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("cmd_goto"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("cmd_if"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("cmd_input"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("cmd_let"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("cmd_lineinput"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("cmd_list"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("cmd_loop"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("cmd_new"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("cmd_next"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("cmd_null"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("cmd_on"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("cmd_print"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("cmd_randomize"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("cmd_read"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("cmd_restore"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("cmd_return"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("cmd_run"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("cmd_troff"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("cmd_tron"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("cmd_trace"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("cmd_const"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("cmd_select"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("cmd_case"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("op_invalid"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("op_exp"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("op_mul"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("op_div"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("op_divint"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("op_add"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("op_subtract"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("op_mod"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("op_ne"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("op_gte"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("op_lte"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("op_lt"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("op_gt"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("op_equal"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("op_and"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("op_or"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("op_xor"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("op_not"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("op_shiftleft"), 0, 0, Null_Address),
+                              (To_Unbounded_String ("op_shiftright"), 0, 0, Null_Address),
                               (To_Unbounded_String (""), 0, 0, Null_Address));
 
 end Commands_And_Tokens_Tables;
