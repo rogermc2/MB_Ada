f677a592bb0ea8d8667dc93c7e272f581045f01a
Roger Mc Murtrie <rogermc@grapevine.com.au>
2023-12-11 23:13:56 +1100

    Update

--- Test/src/main.c
+++ Test/src/main.c
@@ -1,36 +1,54 @@
-
+//  long int strtol(const char *str, char **endptr, int base)
+//  converts the initial part of the string in str to a long int value
+//  according to the given base which must be between 2 and 36 inclusive
+//  or be the special value 0.
+//  endptr is a pointer to the next character after the converted psrt of the string
 #include <stdio.h>
-#include <ctype.h>
+#include <stdlib.h>  // for strtol
+#include <ctype.h>   // for isxdigit
 
 int main(void)
 {
-  char tp[] = {"C2C4FZ34"};
+/*   char tp[] = {"C2C4F034"}; */
+  char tp[] = {"12C4F03 function"};
   char* p;
   int i;
   p = tp;
 
+  printf ("tp[0]: %d\n", tp[0]);
   for (i = 0; i < 8; i++)
     // is this eight hex digits
     {
-      if (!isxdigit(p[i]))
+      if (!isxdigit(tp[i]))
 	{
-	  printf ("hex test,%d: %d is not a hex digit\n\n", i, p[i]);
+	  printf ("\nhex test,%d: %d is not a hex digit\n", i, tp[i]);
 	  break;
 	}
       else
-	printf ("hex test %d: %d is a hex digit\n", i, p[i]);
-
-      if (isdigit (*p) && i < 8)
-	{   // if it a digit and not an 8 digit hex number (ie, it is CFUNCTION data) then try for a line number
-	  printf ("digit test %d: %d is a digit\n", i, *p);
-	}
-      else
-	{
-	  printf ("\n");
-	  printf ("digit test %d: %d is not a digit\n\n", i, p);
-	}
+	printf ("hex test %d: %d is a hex digit\n", i, tp[i]);
     }
 
-  printf ("%d:\n", i, *tp);
+  printf ("\ni: %d:\n", i);
+
+  if (isdigit (*tp) && i < 8)
+    {   // if it is a digit but not an 8 digit hex number
+        //  (ie, it is CFUNCTION data) then try for a line number
+      printf ("digit test %d: %d is a digit\n", i, *tp);
+      // strtol converts a string to a long integer
+      // tp is the string containing the representation of
+      // an integral number.
+      // &tp is the reference to an object of type char* whose value
+      // is set by the function to the next character in str
+      // after the numerical value.
+      // 10 is the base.
+      printf ("tp: %d\n", *tp);
+      i = strtol(tp, &tp, 10);
+      printf ("after strtol i: %dl, tp: %d\n", i, *tp);
+    }
+  else
+    {
+      printf ("\n");
+      printf ("Either %d is not a digit or i = %d > 7\n\n", *tp, i);
+    }
 
 }
