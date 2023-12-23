da05fe45b2879ba67f323ae8e873fd0e84988fa8
rogermc2 <rogermc@iinet.net.au>
2023-12-23 20:08:36 +1100

    Update

--- src/m_misc.adb
+++ src/m_misc.adb
@@ -4,11 +4,12 @@ with Ada.Characters.Handling;
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 
 with Command_And_Token_Tables; use Command_And_Token_Tables;
+with Console;
 with Flash;
 with M_Basic; use M_Basic;
 package body M_Misc is
 
-   procedure Crunch_Data (Pos : Positive; Num : Natural);
+   procedure Crunch_Data (Pos : Unsigned_Byte_Array; Num : Natural);
 
    procedure Autosave (Current_Line_Ptr : Natural; Command_Line : String) is
       use Ada.Characters.Handling;
@@ -17,7 +18,9 @@ package body M_Misc is
       Temp_Memory  : Unsigned_Byte_Array (1 .. Flash.EDIT_BUFFER_SIZE);
       P_Pos        : Positive := Temp_Memory'First;
       Buff_Pos     : Positive := Temp_Memory'First;
+      aChar        : Character;
       Crunch       : Boolean := False;
+      OK           : Boolean := False;
    begin
       Assert (Current_Line_Ptr = 0, Routine_Name &
                 "Current_Line_Ptr should be 0.");
@@ -27,6 +30,9 @@ package body M_Misc is
       end if;
 
       Clear_Program;
+      Crunch_Data (Temp_Memory, 0);
+
+      OK := Console.Get_Console (aChar);
 
    end Autosave;
 
@@ -36,7 +42,7 @@ package body M_Misc is
 
    end Check_Interrupt;
 
-   procedure Crunch_Data (Pos : Positive; Num : Natural) is
+   procedure Crunch_Data (Pos : Unsigned_Byte_Array; Num : Natural) is
    begin
       null;
 
