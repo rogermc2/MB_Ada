d0663cd47bf30ac7236500acb083d9738c193627
Roger Mc Murtrie <rogermc@grapevine.com.au>
2024-01-24 11:38:51 +1100

    Update

--- src/file_io.adb
+++ src/file_io.adb
@@ -10,6 +10,7 @@ with Arguments;
 with Evaluation;
 --  with External;
 with Flash;
+with Support;
 
 package body File_IO is
 
@@ -24,14 +25,18 @@ package body File_IO is
       use Evaluation;
       Routine_Name : constant String := "File_IO.Config_SD_Card ";
       Arg          : Unbounded_String;
-      Arg_Val      : Integer;
+      Arg_Val      : Integer := 0;
       --        Pin_Check    : Integer;
    begin
       Put_Line (Routine_Name & "Arg_String: " & To_String (Arg_String));
       Get_Args (Arg_String, 1, 5, ",");
       Assert (Arg_V.Length > 0, Routine_Name & "Arg_V is empty");
---        Arg := To_Unbounded_String (Arg_V (1));
-      Arg_Val := Integer (Get_Integer (Arg));
+      Put_Line (Routine_Name & "Arg_C: " & Integer'Image (Arg_C));
+      Put_Line (Routine_Name & "Arg_Buffer: ");
+      Support.Print_Buffer (Arg_Buffer);
+      Arg := To_Unbounded_String (Arg_Buffer (Arg_V (1)));
+--        Arg_Val := Integer (Get_Integer (Arg));
+      Put_Line (Routine_Name & "Arg_Val: " & Integer'Image (Arg_Val));
       Assert (Integer (Arg_C) > 0 and then Integer (Arg_C) <= 5,
               Routine_Name & "invalid number of arguments:" &
                 Integer'Image (Integer (Arg_C)) &
@@ -41,6 +46,7 @@ package body File_IO is
       --        Pin_Check := External.Check_Pin (Arg_1_Val, Commands.Option_Error_Check);
       Flash.Option.SDCARD_CS := Arg_Val;
 
+      Put_Line (Routine_Name & "Arg_Val: " & Integer'Image (Arg_Val));
       if Integer (Arg_C) > 2 then
 --           Arg := To_Unbounded_String (Arg_V (3));
          Arg_Val := Integer (Get_Integer (Arg));
@@ -60,6 +66,7 @@ package body File_IO is
    function File_Get_Character (File_Num : Positive) return Character is
    begin
       return Character'Val (0);
+
    end File_Get_Character;
 
    procedure Init_File_IO is
